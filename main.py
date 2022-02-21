32f456da31ef3b3672a2d0ff987188b420751e1f
jelmerkleine@gmail.com <jelmerkleine@gmail.com>
2022-02-21 17:05:46 +0100

    working as intend without changing file

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 with open('g-code.txt') as f:
     new_line = ""
     for line in f:
-        for i in line:
-            #if i =='e':
-            new_line = new_line + i
-        #print(line)
-        print(new_line)
+        stripped = line.split('E', 1)[0]
+        doubleStripped = stripped.split(';', 1)[0]
+        print(doubleStripped)
+        #print(new_line)
