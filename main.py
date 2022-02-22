e55e324dd8e3e53ad450696567c74cf9f1d839dc
jelmerkleine@gmail.com <jelmerkleine@gmail.com>
2022-02-22 08:52:09 +0100

    changing the 3d printer g-code to cnc friendly g-code is working as intended.

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+new_file = open("test.txt","w")
 with open('g-code.txt') as f:
     new_line = ""
     for line in f:
         stripped = line.split('E', 1)[0]
         doubleStripped = stripped.split(';', 1)[0]
         print(doubleStripped)
+        new_file.write(doubleStripped)
         #print(new_line)
