ecfbeb99c8488b3f965c56dc3ae9dffe31ebd954
jelmerkleine@gmail.com <jelmerkleine@gmail.com>
2022-04-26 15:40:25 +0200

    fixed the output and commented out preious changes to exchnage e with t

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ new_file = open("test.txt","w")
 with open('g-code.txt') as f:
     new_line = ""
     for line in f:
-        filtered = line.replace("E","T")
-        doubleStripped = filtered.split(';', 1)[0]
+        #filtered = line.replace("E","T")
+        stripped = line.split('E', 1)[0]
+        doubleStripped = stripped.split(';', 1)[0]
         print(doubleStripped)
         print("hi")
         new_file.write(doubleStripped+ "\n")
