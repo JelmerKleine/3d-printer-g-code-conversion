1539d86f245a3d2298ab32bca5c88508422f44c1
JelmerKleine <jelmerkleine@gmail.com>
2022-04-29 10:49:02 +0200

    added a way to change E commands to S commands and maaking integgers from  these values.

--- main.py
+++ main.py
@@ -10,11 +10,21 @@ with open('g-code.txt') as f:
         filtered5 = filtered4.split('S= ', 1)[0]
         doubleStripped = filtered5.split(';', 1)[0]
         lhs = doubleStripped.split('=')
-        print(doubleStripped)
+        #print(doubleStripped)
+        #print(lhs[0])
         if(len(lhs) == 2):
-            print("S"+lhs[1])
-
+            #print("="+lhs[1])
+            lhs2 = lhs[1].split('F')
+            newLine = lhs[0]
+            if(len(lhs2) == 2):
+                #print("F" + lhs2[1])
+                newLine = newLine + "=" + str(float(lhs2[0])*100000) + " F" + lhs2[1]
+            else:
+                newLine = newLine + "=" + str(float(lhs2[0])*100000)
+        else:
+            newLine = lhs[0]
+        print(newLine)
         #print(rhs)
-        print("hi")
-        new_file.write(doubleStripped+ "\n")
+        #print("hi")
+        new_file.write(newLine  + "\n")
         #print(new_lind)
