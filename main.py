23fd5a08a829180666fa4a42cd4a16475cf784a6
jelmerkleine@gmail.com <jelmerkleine@gmail.com>
2022-05-16 15:35:24 +0200

    converting extruder values to position of auxillary axis.

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
-new_file = open("test.txt","w")
+import math
+
+new_file = open("test_3.nc","w")
 with open('g-code.txt') as f:
     new_line = ""
     for line in f:
         stripped = line.split('S', 1)[0]
-        filtered = stripped.replace("E","S=")
-        filtered2 = filtered.split('G92 ', 1)[0]
-        filtered3 = filtered2.split('G28 ', 1)[0]
-        filtered4 = filtered3.split('M84', 1)[0]
-        filtered5 = filtered4.split('S= ', 1)[0]
-        doubleStripped = filtered5.split(';', 1)[0]
+        stripped2 = stripped.replace("E","Q1=")
+        stripped3 = stripped2.replace("G92","M01")
+        filtered = stripped3.split('G28', 1)[0]
+        filtered2 = filtered.split('M84', 1)[0]
+        filtered3 = filtered2.split('S= ', 1)[0]
+        doubleStripped = filtered3.split(';', 1)[0]
         lhs = doubleStripped.split('=')
         #print(doubleStripped)
         #print(lhs[0])
@@ -18,9 +20,9 @@ with open('g-code.txt') as f:
             newLine = lhs[0]
             if(len(lhs2) == 2):
                 #print("F" + lhs2[1])
-                newLine = newLine + "=" + str(float(lhs2[0])*100000) + " F" + lhs2[1]
+                newLine = newLine + "=" + str(float(lhs2[0])/((math.pi*math.pow(1.75,2))/4)) + " F" + lhs2[1] #*100000
             else:
-                newLine = newLine + "=" + str(float(lhs2[0])*100000)
+                newLine = newLine + "=" + str(float(lhs2[0])/((math.pi*math.pow(1.75,2))/4))
         else:
             newLine = lhs[0]
 
