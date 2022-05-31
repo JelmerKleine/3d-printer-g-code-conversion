abe7b4926d38f88a706eb4e2c30575c48869e15e
jelmerkleine@gmail.com <jelmerkleine@gmail.com>
2022-05-31 13:50:31 +0200

    added a m01 function again

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import math
 
-new_file = open("test_3_segment_1.nc","w")
-new_file_2 = open("test_3_segment_2.nc","w")
+new_file = open("test_4.nc", "w")
 with open('g-code.txt') as f:
     new_line = ""
     segment = 0
     for line in f:
         stripped = line.split('S', 1)[0]
-        stripped2 = stripped.replace("E","Q1=")
-        filtered = stripped2.split('G28', 1)[0]
+        stripped2 = stripped.replace("E","H=")
+        stripped3 = stripped2.replace("G92","MO1")
+        filtered = stripped3.split('G28', 1)[0]
         filtered2 = filtered.split('M84', 1)[0]
         filtered3 = filtered2.split('S= ', 1)[0]
         filtered4 = filtered3.split('M140', 1)[0]
@@ -32,9 +32,9 @@ with open('g-code.txt') as f:
             newLine = lhs[0]
             if(len(lhs2) == 2):
                 #print("F" + lhs2[1])
-                newLine = newLine + "=" + str(float(lhs2[0]) + " F" + lhs2[1] #/((math.pi*math.pow(1.75,2))/4)
+                newLine = newLine + "=" + str(float(lhs2[0])) + " F" + lhs2[1] #/((math.pi*math.pow(1.75,2))/4)
             else:
-                newLine = newLine + "=" + str(float(lhs2[0]) #/((math.pi*math.pow(1.75,2))/4)
+                newLine = newLine + "=" + str(float(lhs2[0])) #/((math.pi*math.pow(1.75,2))/4)
         else:
             newLine = lhs[0]
 
@@ -43,8 +43,8 @@ with open('g-code.txt') as f:
             print("test")
 
 
-        new_file_2.write(newLine + "\n")
-        print(newLine)
+        new_file.write(newLine + "\n")
+
         #print(rhs)
         #print("hi")
         #if segment == 1 :
