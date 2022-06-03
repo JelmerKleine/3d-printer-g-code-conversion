52b832bd7a07e405e30a7fb7e628ed51c30abfb0
jelmerkleine@gmail.com <jelmerkleine@gmail.com>
2022-06-03 12:10:26 +0200

    scripts don't need any manual changs anymore

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import math
 
-new_file = open("test_4.nc", "w")
-with open('g-code.txt') as f:
+new_file = open("calubrationCube.nc", "w")
+with open('CE6_xyzCalibration_cube.gcode') as f:
     new_line = ""
     segment = 0
     for line in f:
         stripped = line.split('S', 1)[0]
         stripped2 = stripped.replace("E","H=")
-        stripped3 = stripped2.replace("G92","MO1")
+        stripped3 = stripped2.replace("G92","M01")
         filtered = stripped3.split('G28', 1)[0]
         filtered2 = filtered.split('M84', 1)[0]
         filtered3 = filtered2.split('S= ', 1)[0]
@@ -29,14 +29,18 @@ with open('g-code.txt') as f:
         if(len(lhs) == 2):
             #print("="+lhs[1])
             lhs2 = lhs[1].split('F')
+            lhs3 = lhs[1].split('Z')
             newLine = lhs[0]
-            if(len(lhs2) == 2):
-                #print("F" + lhs2[1])
-                newLine = newLine + "=" + str(float(lhs2[0])) + " F" + lhs2[1] #/((math.pi*math.pow(1.75,2))/4)
-            else:
-                newLine = newLine + "=" + str(float(lhs2[0])) #/((math.pi*math.pow(1.75,2))/4)
+            if(len(lhs3) == 2): # if a z has been encounterd after an E command
+                # E command is multiplied with 100000 and the rest o the command is pasted after
+                newLine = newLine + "=" + str(float(lhs3[0]) * 100000) + " Z" + lhs3[1]  # /((math.pi*math.pow(1.75,2))/4)
+            elif(len(lhs2) == 2):#print("F" + lhs2[1]) # if a F has been encounterd after an E command
+                # E command is multiplied with 100000 and the rest o the command is pasted after
+                newLine = newLine + "=" + str(float(lhs2[0])*100000) + " F" + lhs2[1] #/((math.pi*math.pow(1.75,2))/4)
+            else: # no letters after the E commands
+                newLine = newLine + "=" + str(float(lhs2[0])*100000) #/((math.pi*math.pow(1.75,2))/4)
         else:
-            newLine = lhs[0]
+            newLine = lhs[0] # no E command
 
         if (newLine.find("M") != -1):
             segment = segment + 1
